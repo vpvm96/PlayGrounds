75e01fe1d49e6bb05597172272f645f1a65d58b6
vpvm96 <vpvm96@gmail.com>
2021-09-03 18:17:42 +0900

    playGrounds06

--- homework/note.js
+++ homework/note.js
@@ -20,7 +20,6 @@
 // console.log(calculate('multiply', 10, 10));
 // console.log(calculate('remainder', 50, 2));
 
-
 //----------------------------------------------------
 
 // 1. 100개의 랜덤숫자를 만들고 요소 합을 구하시오.
@@ -55,7 +54,7 @@
 
 // 1. a = 5, b = 3 두 수의 교환.
 // 2. 1 ~ 10 까지 랜덤 -> 최대 최소 찾고 두 요소 교환.
-// 3. 1 ~ 10 까지 랜덤 수 -> 정렬 .sort() 사용 x 
+// 3. 1 ~ 10 까지 랜덤 수 -> 정렬 .sort() 사용 x
 
 // const randomArr = [];
 
@@ -168,3 +167,61 @@
 
 // ------------------------------------------------------------------
 
+// const randomArr = [];
+
+// const randomNum = (randomArr) => {
+//   for (let i = 0; i < 10; i++) {
+//     randomArr[i] = Math.floor(Math.random() * 10 + 1);
+//   }
+
+//   console.log(randomArr);
+
+//   let max = randomArr[0];
+//   let maxIndex = 0;
+
+//   for (let i = 1; i < randomArr.length; i++) {
+//     if (max < randomArr[i]) {
+//       max = randomArr[i];
+//       maxIndex = i;
+//     }
+//   }
+
+//   let min = randomArr[0];
+//   let minIndex = 0;
+
+//   for (let i = 1; i < randomArr.length; i++) {
+//     if (min > randomArr[i]) {
+//       min = randomArr[i];
+//       minIndex = i;
+//     }
+//   }
+
+//   swap(maxIndex, minIndex, randomArr);
+// };
+
+// const swap = (a, b, randomArr) => {
+//   let swapNum = a;
+//   a = b;
+//   b = swapNum;
+//   console.log(`바뀔 숫자는: ${+randomArr[a] + ' ' + randomArr[b]}`);
+// };
+// randonNum(randomArr);
+
+// ---------------------------------------------------------------
+
+// 부족한 금액 계산하기
+
+// function solution(price, money, count) {
+//   let result = 0;
+
+//   for (let i = 1; i <= count; i++) {
+//     result += price * i;
+//   }
+
+//   if (result < money) result = 0;
+//   else result -= money;
+
+//   return result;
+// }
+
+// ----------------------------------------------------------------
---
+++ react2/shop/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button type="button" class="btn btn-primary">Primary</button>
+    </div>
+  );
+}
+
+export default App;
