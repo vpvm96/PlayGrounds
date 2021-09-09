3dec6e2581523c113f470c64d8d92ced563208cb
vpvm96 <vpvm96@gmail.com>
2021-09-09 15:45:56 +0900

    playGrounds07

---
+++ react2/shop/src/content.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import Data from './data.js';
+
+function Content () {
+  let [shoes, shoes변경] = useState(Data);
+
+  return (
+  <div className ='container'>
+      <div className ='row'>
+          {shoes.map((sho, i) => {
+            return(
+              <div className ='col-md-4' key={i}>
+                <img src= { sho.url } width ='100%'></img>
+                <h4> { sho.title }</h4>
+                <p>{ sho.content } & { sho.price }</p>
+              </div>
+            );
+          })} 
+      </div>
+  </div>
+  );
+};
+
+export default Content;
