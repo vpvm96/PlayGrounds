1866877bd5652b2ca6578590f6b0eeabe26f8f23
vpvm96 <vpvm96@gmail.com>
2021-12-14 16:42:14 +0900

    fix

--- react2/shop/src/App.js
+++ react2/shop/src/App.js
@@ -19,7 +19,7 @@ function App() {
     <div className="App">
       <Navbar bg="dark" variant="dark">
         <Container>
-        <Navbar.Brand href="#home">Edit Shop</Navbar.Brand>
+        <Navbar.Brand href="/">Edit Shop</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link as={Link} to='/'>Home</Nav.Link>
           <Nav.Link as={Link} to='/cart'>Cart</Nav.Link>
