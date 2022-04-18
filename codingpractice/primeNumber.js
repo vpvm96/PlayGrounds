8be0bf4aa147a05274a7e03cf86328b9c9b6afab
vpvm96 <vpvm96@gmail.com>
2022-04-18 18:10:04 +0900

    fix primNumber

--- codingpractice/primeNumber.js
+++ codingpractice/primeNumber.js
@@ -40,6 +40,35 @@
   console.log(solution(2, 50));
 }
 
+{ // 에라스토텔레스의 체
+  const input = [3, 16];
+  const N = input[0];
+  const M = input[1];
+
+  let isPrimeNum = Array(M + 1).fill(true);
+  isPrimeNum[0] = isPrimeNum[1] = false;
+
+  function solution() {
+    for (let i = 2; i <= Math.ceil(Math.sqrt(M)); i++) {
+      if (isPrimeNum[i]) {
+        let m = 2;
+        while (i * m <= M) {
+          isPrimeNum[i * m] = false;
+          m++;
+        }
+      }
+    }
+    const result = [];
+    for (let i = N; i <= M; i++) {
+      if (isPrimeNum[i]) {
+        result.push(i);
+      }
+    }
+    console.log(result.join('\n'));
+  }
+  solution();
+}
+
 // const print = () => {
 //   if (end == 10) {
 //     return;
