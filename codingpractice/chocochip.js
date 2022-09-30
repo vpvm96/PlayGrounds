7ac57dae8d1b65154d3f6a2aa2727337505aff75
vpvm96 <vpvm96@gmail.com>
2022-09-30 15:42:40 +0900

    Practice: Coding Test

--- codingpractice/chocochip.js
+++ codingpractice/chocochip.js
@@ -1,21 +1,30 @@
-let choco = "촉촉한 초코칩";
+let choco = "촉촉한 초코칩"
 let test = "안 촉축한 초코칩은 맛있다 촉촉한 초코칩"
 
-function solution () {
-  let strLen = choco.length;
-  let cnt = 0;
+function solution() {
+    let strLen = choco.length
+    let cnt = 0
 
-  for (let i = 0; i < test.length; i++) {
-    if (test[i] == "촉") {
-      let j = i;
-      if (test.substring(j, strLen + j).length < test.length) {
-        let comp = test.substring(j, strLen + j);
-        if (comp === choco) {
-          cnt++;
-          console.log(cnt);
+    for (let i = 0; i < test.length; i++) {
+        if (test[i] == "촉") {
+            let j = i
+            if (test.substring(j, strLen + j).length < test.length) {
+                let comp = test.substring(j, strLen + j)
+                if (comp === choco) {
+                    cnt++
+                    console.log(cnt)
+                }
+            }
         }
-      }
     }
-  }
 }
-solution();
+solution()
+
+function solution() {
+    let choco = "촉촉한 초코칩"
+    let test = "안 촉축한 초코칩은 맛있다 촉촉한 초코칩"
+
+    return test.split(choco).length - 1
+}
+
+console.log(solution())
