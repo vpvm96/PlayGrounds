0b4660a51f8a0196e33f18841ef60e7a1caf8144
vpvm96 <vpvm96@gmail.com>
2022-10-03 20:32:38 +0900

    Practice: Coding

--- homework/sameNum.js
+++ homework/sameNum.js
@@ -3,38 +3,49 @@
 //    arr = [4, 4, 4, 3, 3] -> return:[4, 3]
 //    Solutuion: Fill the function of solution
 
-// function solution(arr) {
-//  var answer = [];
-//  return answer;
-//}
+function solution(arr) {
+    var answer = []
+    return answer
+}
 
-// function solution() {
-//   var answer = [];
-//   var arr = [1, 1, 3, 3, 0, 1, 1];
+function solution() {
+    var answer = []
+    var arr = [1, 1, 3, 3, 0, 1, 1]
 
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i - 1] !== arr[i]) {
-//       answer.push(arr[i]);
-//     }
-//   }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i - 1] !== arr[i]) {
+            answer.push(arr[i])
+        }
+    }
 
-//   return answer;
-// }
-// console.log(solution());
+    return answer
+}
+console.log(solution())
 
 // 1 ~ 100까지 숫자중 홀수의 경우 "수" 짝수의 경우 "박"을 출력
 
-const solution = () => {
-  let answer = "";
+function solution5(arr) {
+    let newArr = []
 
-  for(let i = 1; i < 100; i++) {
-    if(i % 2 === 1) {
-      answer += "수";
-    }else {
-      answer += "박";
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== arr[i - 1]) newArr.push(arr[i])
     }
-  }
-  return answer;
+    return newArr
 }
 
-console.log(solution());
+console.log(solution5([1, 1, 3, 3, 0, 1, 1, 2, 1]))
+
+const solution = () => {
+    let answer = ""
+
+    for (let i = 1; i < 100; i++) {
+        if (i % 2 === 1) {
+            answer += "수"
+        } else {
+            answer += "박"
+        }
+    }
+    return answer
+}
+
+console.log(solution())
