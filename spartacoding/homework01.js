965d7dca54b0371e2923fb0cdcb30a286a6241f1
vpvm96 <vpvm96@gmail.com>
2022-10-14 12:04:40 +0900

    boostcamp 강의, 숙제

--- spartacoding/homework01.js
+++ spartacoding/homework01.js
@@ -2,30 +2,71 @@ const nameInput = document.querySelector("#floatingInput")
 const descInput = document.querySelector("#floatingTextarea2")
 const submitBtn = document.querySelector("#submitBtn")
 const card = document.querySelector("#mypost_card")
+const tempString = document.querySelector("#temp")
 
-submitBtn.addEventListener("click", () => {
-  const inputValue = []
+function useState(initialValue) {
+  let value = initialValue
+  const state = () => value
 
-  const obj = {}
-  obj.name = nameInput.value
-  obj.desc = descInput.value
-
-  if (obj.name !== "" && obj.desc !== "") {
-    inputValue.push(obj)
-  } else {
-    alert("내용을 입력 하세요")
+  function setState(newValue) {
+    value = newValue || state()
   }
 
+  return [state, setState]
+}
+
+function createCardInformation(obj) {
+  const [myState, setMyState] = useState({
+    name: "",
+    desc: "",
+  })
+  setMyState({
+    name: obj.name,
+    desc: obj.desc,
+  })
+  const { name, desc } = myState()
   card.innerHTML += `
         <div class="card">
             <div class="card-body">
                 <blockquote class="blockquote mb-0">
-                    <p>${inputValue[0].desc}</p>
-                    <footer class="blockquote-footer">${inputValue[0].name}</footer>
+                    <p>${desc}</p>
+                    <footer class="blockquote-footer">${name}</footer>
                 </blockquote>
             </div>
         </div>
     `
+}
+
+submitBtn.addEventListener("click", () => {
+  const obj = {}
+  obj.name = nameInput.value
+  obj.desc = descInput.value
+  if (!obj.name || !obj.desc) {
+    alert("내용을 입력 하세요")
+    return
+  }
+  createCardInformation(obj)
   nameInput.value = ""
   descInput.value = ""
 })
+
+function showTempRequest() {
+  $.ajax({
+    type: "GET",
+    url: "http://spartacodingclub.shop/sparta_api/weather/seoul",
+    data: {},
+    success: function (response) {
+      const { temp } = response
+
+      tempString.innerHTML += `
+                현재기온 : <span>${temp}</span> 도
+            `
+    },
+  })
+}
+
+showTempRequest()
+
+// $(document).ready(function(){
+// 	alert('다 로딩됐다!')
+// });
