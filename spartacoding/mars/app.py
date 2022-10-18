574be3625562720aa9e8c89f980933cfb0ec23da
vpvm96 <vpvm96@gmail.com>
2022-10-18 10:57:54 +0900

    boostcamp: project prac

--- spartacoding/mars/app.py
+++ spartacoding/mars/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -10,11 +9,25 @@ def home():
 
 @app.route("/mars", methods=["POST"])
 def web_mars_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'POST 연결 완료!'})
+    name_receive = request.form['name_give']
+    address_receive = request.form['address_give']
+    size_receive = request.form['size_give']
+    doc = {
+        "name": name_receive,
+        "address": address_receive,
+        "size": size_receive,
+    }
+    db.mars.insert_one(doc)
+
+    return jsonify({'msg': '주문 완료!'})
 
 
 @app.route("/mars", methods=["GET"])
 def web_mars_get():
-    return jsonify({'msg': 'GET 연결 완료!'})
+    order_list = list(db.mars.find({}, {'_id': False}))
+    return jsonify({'orders': order_list})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
+
